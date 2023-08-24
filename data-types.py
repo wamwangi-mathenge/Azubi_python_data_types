4cead28065a98282b9b41c7f1fb59d93bc9d9162
Brian <wamwangimathenge@gmail.com>
2023-08-24 12:58:38 +0300

    Perform different string modifications

---
+++ data-types.py
@@ -0,0 +1,16 @@
+# Working with strings
+print("My name is Brian")
+
+# Defining your variables
+law = "Happy birthday to you all"
+print(len(law))
+
+# Modify strings
+b = "we are learning python data types"
+print(b.upper())
+
+# String concatenation
+a = "Beautiful"
+b = "day"
+c = a + " " + b
+print(c)
