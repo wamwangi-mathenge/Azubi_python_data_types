ed093d41289f57a64f7636de8dfde832032987b6
Brian <wamwangimathenge@gmail.com>
2023-08-24 13:33:00 +0300

    Perform basic list operations

--- data-types.py
+++ data-types.py
@@ -14,3 +14,29 @@ a = "Beautiful"
 b = "day"
 c = a + " " + b
 print(c)
+
+# List Data Types
+students = ["Micah", "Sharon", "Nassir", "Mac", "Samuel", "Rahab", "Brian"]
+print(students)
+
+# Access list items
+print(students[0])
+print(students[-1])
+print(students[2:5])
+print(students[:4])
+
+# Changing list items
+students[1] = "Gustave"
+print(students)
+
+# Insert items
+students.insert(2, "Caleb")
+print(students)
+
+students.append("Faith")
+print(students)
+
+students.remove("Mac")
+print(students)
+
+
